9dd88233049d26d18d3c0cc384c3a57f85a5c8ab
RishabhBatra-2000 <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-10-16 23:22:30 +0530

    Create link list.py

---
+++ link list.py
@@ -0,0 +1,16 @@
+def __init__(self, nodes=None):
+    self.head = None
+    if nodes is not None:
+        node = Node(data=nodes.pop(0))
+        self.head = node
+        for elem in nodes:
+            node.next = Node(data=elem)
+            node = node.next
+
+def __iter__(self):
+    node = self.head
+    while node is not None:
+        yield node
+        node = node.next
+
+        
