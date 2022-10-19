1b580c6b08c1bea6dfdc2ca9302008a5cf632f93
RishabhBatra-2000 <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-10-19 23:33:25 +0530

    Update link list.py

--- python/link list.py
+++ python/link list.py
@@ -3,17 +3,10 @@ from structlinks import LinkedList
 
 
 lst = LinkedList()
-
-
 lst = LinkedList([1, 10.0, 'string'])
 
 print(lst)
-
-print()
-
 print('Elements of list:')
-
-
 element0 = lst[0]
 element1 = lst[1]
 element2 = lst[2]
@@ -22,38 +15,21 @@ print(f'first element : {element0}')
 print(f'second element : {element1 }')
 print(f'third element : {element2}')
 
-print()
-
 print('Length of list:')
-
-
 length = len(lst)
 
 print(f'size of the list : {length}')
-
-print()
-
 print('Set item:')
 
-
 lst[0] = 10
-
 print(f'list after setting lst[0] to 10 : {lst}')
 
-print()
-
 print('Append And Insert:')
-
-
 lst.append('another string')
 lst.insert(1, 0.0)
 
 print(f'list after appending and inserting: {lst}')
-
-print()
-
 print('Pop and Remove')
-
 
 element = lst.pop(0)
 lst.remove(10.0)
