b30c77599a1036c1ba5bfbdb4ae3258880687ec7
RishabhBatra-2000 <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-10-21 22:15:21 +0530

    Update binary tree traversal.java

--- java/binary tree traversal.java
+++ java/binary tree traversal.java
@@ -34,7 +34,7 @@ class BinaryTree {
 		tree.root.right = new Node(3);
 		tree.root.left.left = new Node(4);
 		tree.root.left.right = new Node(5);
-		System.out.println("\nInorder traversal of binary tree is ");
+		System.out.println("\ntraversal of binary tree is ");
 		tree.printtree();
 	}
 }
