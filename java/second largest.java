43f44d7c974e88e10429f3a32e744d0cdd5bb6d4
RishabhBatra-2000 <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-10-26 22:45:50 +0530

    Create second largest.java

---
+++ java/second largest.java
@@ -0,0 +1,28 @@
+    import java.util.Scanner;  
+    public class la  
+    {  
+    public static int large(int[] a, int total){  
+    int temp;  
+    for (int i = 0; i < total; i++)   
+            {  
+                for (int j = i + 1; j < total; j++)   
+                {  
+                    if (a[i] > a[j])   
+                    {  
+                        temp = a[i];  
+                        a[i] = a[j];  
+                        a[j] = temp;  
+                    }  
+                }  
+            }  
+           return a[total-2];  
+    }  
+    public static void main(String[] args)   
+    {  
+    int a[]={1,2,5,6,3,2};  
+    int b[]={44,66,99,77,33,22,55};  
+    System.out.println("Largest: "+large(a,6));  
+    System.out.println("Largest: "+large(b,7));
+    
+    }  
+    }  
