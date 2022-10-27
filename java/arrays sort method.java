498285443897d7cfe08ff7faee970a2a6c5c4a80
RishabhBatra-2000 <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-10-27 23:22:33 +0530

    Update arrays sort method.java
    sorting array using predefined method

--- java/arrays sort method.java
+++ java/arrays sort method.java
@@ -1,9 +1,11 @@
     import java.util.Arrays;   
-    public class sr  {   
+    public class sortingarray  {   
     public static void main(String[] args)   {   
     int [] array = new int [] {90, 23, 5, 109, 12, 22, 67, 34};  
     Arrays.sort(array);   
     System.out.println("Elements of array : ");  
     for (int i = 0; i < array.length; i++)   {       
     System.out.println(array[i]);   
-    }   }  }  
+    }  
+    } 
+    }  
