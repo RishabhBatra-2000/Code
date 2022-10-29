9502eddf323ff9c29e0bdb162e015b2484cc1e72
RishabhBatra-2000 <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-10-29 22:01:36 +0530

    Update characters present in text file.java
    a java program to count the ascii characters in a text file.

--- java/characters present in text file.java
+++ java/characters present in text file.java
@@ -6,16 +6,15 @@
         public static void main(String[] args) throws Exception {  
             String line;  
             int count = 0;  
-      
+            //file reader to read the file and buffer reader to read the file reader variable.
             FileReader file = new FileReader("data.txt");
             BufferedReader br = new BufferedReader(file);  
             System.out.println(" \n");
             while((line = br.readLine()) != null) {  
                 String words[] = line.split("");  
                 count = count + words.length;  
-      
+
             }  
-      
             System.out.println("Number of Character present in given file: " + count);  
             br.close();  
         }  
