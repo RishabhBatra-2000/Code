9d56eb0ed61691ec1e818d95234b9bf712df1487
Rishabh Batra <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-12-27 19:14:54 +0530

    Create basic applet example.java

---
+++ java/basic applet example.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class BasicAppletExample extends Applet{
+ 
+ public void paint(Graphics g){
+ g.drawString("This is my First Applet",20,100);
+ }
+}
