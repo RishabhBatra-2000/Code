794b078e05a7513c10e23e48f24146cd379b5c6b
Rishabh Batra <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-12-28 23:38:39 +0530

    Create get applet context example.java

---
+++ java/get applet context example.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.applet.AppletContext;
+ 
+public class GetAppletContextExample extends Applet{
+ 
+ public void init(){
+ 
+ AppletContext appContext = getAppletContext();
+ 
+ }
+}
