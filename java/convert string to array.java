88b1fde8436c68c3750d4a497853ae161b20ba18
Rishabh Batra <96906901+RishabhBatra-2000@users.noreply.github.com>
2022-12-30 23:27:35 +0530

    Create convert string to array.java

---
+++ java/convert string to array.java
@@ -0,0 +1,18 @@
+public class StringToCharacterArrayExample {
+ 
+  public static void main(String args[]){
+
+    String strOrig = "Hello World";
+
+    char[] stringArray;
+    
+
+    stringArray = strOrig.toCharArray(); 
+    
+
+    for(int index=0; index < stringArray.length; index++)
+      System.out.print(stringArray[index]);
+    
+  }
+  
+}
